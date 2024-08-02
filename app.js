b812610bd05fdd832bf71ad0f5171506bb6a33c0
Nguyễn Duy Thắng <duythang020822@gmail.com>
2024-08-02 13:18:50 +0700

    update api path

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
-app.use("/api", indexRouter);
+app.use("/", indexRouter);
 
 mongoose
   .connect(MongoURI)
